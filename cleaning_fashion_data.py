1d1713b181e40b106c881732fa2e56aee4669714
Misheel31 <misheelrai7@gmail.com>
2025-07-19 13:18:57 +0545

    data cleaning

---
+++ cleaning_fashion_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv('synthetic_online_retail_data.csv')
+
+fashion_df = df[df['category_name']== 'Fashion'].copy()
+
+# Check missing values
+print("\nMissing Values Summary:")
+print(fashion_df.isnull().sum())
+
+# Fix missing values safely using .loc
+fashion_df.loc[:, 'review_score'] = fashion_df['review_score'].fillna(fashion_df['review_score'].mean())
+fashion_df.loc[:, 'gender'] = fashion_df['gender'].fillna('Unknown')
+
+# Convert order_date to datetime format safely
+fashion_df.loc[:, 'order_date'] = pd.to_datetime(fashion_df['order_date'])
+
+# Create total_price column safely
+fashion_df.loc[:, 'total_price'] = fashion_df['price'] * fashion_df['quantity']
